11ce0a7ce054b0f4aa3c638ea5611a6c79e91a17
EdithRosas <3rofloedith@gmail.com>
2025-02-06 14:26:06 -0600

    Add files via upload

---
+++ Modificadores2/src/main/java/com/mycompany/modificadores2/Modificadores2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modificadores2;
+
+import OtrasClases.OtraClase;
+
+/**
+ *
+ * @author Edith Esmeralda
+ */
+public class Modificadores2 {
+
+    public static void main(String[] args) {
+        OtraClase otraClase = new OtraClase();
+        int valorPublico = otraClase.valorPublico;
+                
+    }
+}
