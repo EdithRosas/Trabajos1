525445d57355e3201e757b7927b716caf485e52e
EdithRosas <3rofloedith@gmail.com>
2025-02-27 21:38:54 -0600

    Add files via upload

---
+++ EVA1_5_TV/src/eva1_5_tv/EVA1_5_TV.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_tv;
+
+/**
+ *
+ * @author Edith Esmeralda
+ */
+public class EVA1_5_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Television tv = new Television();
+        tv.imprimirDatos();
+        
+        tv.encenderApagar();
+        
+        tv.subirVolumen();
+        tv.masCanal();
+        tv.masCanal();
+        tv.masCanal();
+        tv.imprimirDatos();
+    }
+    
+}
